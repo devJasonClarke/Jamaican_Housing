d4fe42a7cdd17fe4fae7445db9218afca8bf2fd5
devJasonClarke <jettison200@gmail.com>
2021-06-11 20:53:21 -0500

    applying a fix to the dark theme bug

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,40 +1,34 @@
-import colors from 'vuetify/es5/util/colors'
+import colors from "vuetify/es5/util/colors";
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: 'Jamaican Housing',
-    title: 'Jamaican Housing',
+    titleTemplate: "Jamaican Housing",
+    title: "Jamaican Housing",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   loading: {
-    color: '#00C853',
-    height: '5px'
+    color: "#00C853",
+    height: "5px"
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '~/assets/scss/main.scss'
-  ],
+  css: ["~/assets/scss/main.scss"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    { src: '@/plugins/vueAwesomeSwiper', mode: 'client' }
-  ],
+  plugins: [{ src: "@/plugins/vueAwesomeSwiper", mode: "client" }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -42,17 +36,17 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
+    "@nuxtjs/vuetify"
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
+    "@nuxtjs/pwa"
     // https://go.nuxtjs.dev/content
-/*     '@nuxt/content', */
+    /*     '@nuxt/content', */
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -61,7 +55,7 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
+      lang: "en"
     }
   },
 
@@ -70,11 +64,13 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ["~/assets/variables.scss"],
     theme: {
-      dark: false,
+      options: {
+        customProperties: true
+      },
+
       themes: {
-        
         light: {
           primary: colors.lightBlue,
           secondary: colors.grey.darken1,
@@ -82,22 +78,21 @@ export default {
           error: colors.red.accent3,
           background: colors.indigo.lighten5,
           info: colors.teal.darken1,
-          backgroundShade: '#f8f9fa',
-          backgroundShadeDarker: '#f0f1f3'
+          backgroundShade: "#f8f9fa",
+          backgroundShadeDarker: "#f0f1f3"
         },
         dark: {
           primary: colors.blue.darken4,
           background: colors.indigo.base,
           info: colors.teal.lighten1,
-          backgroundShade: '#272727',
-          backgroundShadeDarker: '#121212'
-        },
+          backgroundShade: "#272727",
+          backgroundShadeDarker: "#121212"
+        }
       },
-    },
-   
+      dark: false
+    }
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {}
+};
