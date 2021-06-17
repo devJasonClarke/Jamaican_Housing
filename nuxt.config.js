1624698cebc03c217d7ef3fd85e47bd1bf98a71a
devJasonClarke <jettison200@gmail.com>
2021-06-16 19:01:01 -0500

    deleted the icon cache in the node_modules

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,8 +11,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: "Jamaica Housing",
-    title: "Jamaica Housing",
+    titleTemplate: "Jamaica Housing ",
+    title: "Jamaica Housing ",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
