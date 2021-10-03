4b25c95041695d4837b7e29d8f5aafcaab7f5a98
devJasonClarke <jettison200@gmail.com>
2021-10-03 15:38:44 -0500

    fixed phonenumber on profile page

--- pages/profile/_slug.vue
+++ pages/profile/_slug.vue
@@ -457,11 +457,11 @@ export default {
     formattedNumber() {
       /* var phone = this.phoneNumber.toString().replace(/(\d{4})(\d{3})(\d{4})/, '$1 $2 $3'); */
       var phone = [
-        this.phoneNumber.toString().slice(0, 3),
+        this.contact.phoneNumber.toString().slice(0, 3),
         "-",
-        this.phoneNumber.toString().slice(3, 6),
+        this.contact.phoneNumber.toString().slice(3, 6),
         "-",
-        this.phoneNumber.toString().slice(6)
+        this.contact.phoneNumber.toString().slice(6)
       ].join("");
       return phone;
     }
