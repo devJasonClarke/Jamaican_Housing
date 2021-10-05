33e45d7b3ce8aab18913816ed5a62c9cfaddbe89
devJasonClarke <jettison200@gmail.com>
2021-10-05 15:48:49 -0500

    added featured listings skeleton loader

--- components/TheSwiper/TheSwiperPropertyFeaturedFirebase.vue
+++ components/TheSwiper/TheSwiperPropertyFeaturedFirebase.vue
@@ -1,6 +1,35 @@
 <template>
   <div class="section-margin-top ">
-    <swiper class="swiper" :options="propertySwiperOption">
+    <swiper class="swiper" :options="propertySwiperOption" v-if="loading">
+      <swiper-slide class="sliide" v-for="index in 7" :key="index">
+        <v-card outlined class="pa-3 rounded-lg">
+          <v-skeleton-loader
+            class="mx-auto "
+            type="image,article"
+          ></v-skeleton-loader>
+        </v-card>
+      </swiper-slide>
+
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+    <v-container class="px-6 px-sm-0 py-3" v-else-if="!properties.length">
+      <SectionPadding class="py-0 ">
+        <v-sheet
+          height="200px"
+          class="d-flex justify-center align-center flex-column pa-3"
+          outlined
+          ><p class="text-body-1 text-sm-h6 text-center font-weight-regular">
+            There are no featured properties as yet.
+            <br />This is the perfect time to feature yours!<!--  Add your first propery today! -->
+          </p>
+          <v-btn nuxt :to="{ name: 'contact' }" dark color="green accent-4">
+            <v-icon left>mdi-home-import-outline</v-icon> Feature
+            Property</v-btn
+          >
+        </v-sheet>
+      </SectionPadding>
+    </v-container>
+    <swiper class="swiper" :options="propertySwiperOption" v-else>
       <swiper-slide
         class="sliide"
         v-for="(property, index) in properties"
